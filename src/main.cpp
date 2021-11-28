0e20313ed56903334d60a150114331046971a7f7
ishikagovil <ishikagovil@gmail.com>
2021-11-28 13:22:43 -0500

    working fluid simulation to output FPS

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,6 @@
 #define SIM_VISC 0
 #define SIM_DIFF 0.0000001f
 
-void renderSim(Sim &sim)
-{
-    std::vector<float> &d = sim.getDensity();
-
-    for (int i = 0; i < SIM_SIZE; i++)
-    {
-        for (int j = 0; j < SIM_SIZE; j++)
-        {
-            int idx = sim.idx(j, i);
-            float val = fmin(1.0, d[idx] / 200.0f);
-            int r = val * 255;
-            int g = val * 255;
-            int b = val * 255;
-            int a = 255;
-
-            int idx2 = (i * SIM_SIZE + j) * 4;
-        }
-    }
-}
-
 int main()
 {
     Sim sim(SIM_SIZE, SIM_SIZE, SIM_DT, SIM_VISC, SIM_DIFF);
@@ -35,26 +15,26 @@ int main()
     // store the previous mouse position
     int prevPosx = 0, prevPosy = 0;
     srand (time(NULL));
-
-    while (1)
-    {
-        int mousePosx = rand() % SIM_SIZE;
-        int mousePosy = rand() % SIM_SIZE;
-        std::cout<<mousePosx <<", "<<mousePosy<<std::endl;
-        sim.addVelocity(mousePosx, mousePosy, (float)(mousePosx - prevPosx) / 10.0, (float)(mousePosy - prevPosy) / 10.0);
-        if (rand() % 10 < 5)
-        {
-            // add fluid density to mouse area
-            sim.addDensity(mousePosx, mousePosy, 200.0f);
+    float fps = 0.0;
+    int count = 0;
+    for (int mousePosx = 0; mousePosx < SIM_SIZE; mousePosx+=3) {
+        for (int mousePosy = 0; mousePosy < SIM_SIZE; mousePosy+=3) {
+            std::cout<<mousePosx <<", "<<mousePosy<<std::endl;
+            sim.addVelocity(mousePosx, mousePosy, (float)(mousePosx - prevPosx) / 10.0, (float)(mousePosy - prevPosy) / 10.0);
+            if (rand() % 10 < 5) {
+                // add fluid density to mouse area
+                sim.addDensity(mousePosx, mousePosy, 200.0f);
+            }
+            current_ticks = clock();
+            sim.step();
+            delta_ticks = clock() - current_ticks;
+            fps += (float) CLOCKS_PER_SEC / delta_ticks;
+            //std::cout << fps << std::endl;
+            prevPosx = mousePosx;
+            prevPosy = mousePosy;
+            count++;
         }
-        current_ticks = clock();
-        sim.step();
-        delta_ticks = clock() - current_ticks;
-        clock_t fps = CLOCKS_PER_SEC / delta_ticks;
-        //std::cout << fps << std::endl;
-        
-        renderSim(sim);
-        prevPosx = mousePosx;
-        prevPosy = mousePosy;
     }
+    fps /= count;
+    std::cout << "FPS " << fps << std::endl;
 }
